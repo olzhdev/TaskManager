e44bf5c9c443efbffa72af12ee00b82de6734bc6
Olzh <@>
2022-11-06 11:27:00 +0600

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by MAC on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
